26d40e0c7a2b69c4fb57db9e8616fa8e733d374f
JBowe-tech <jbowe@email.neit.edu>
2025-01-30 17:08:33 -0500

    wk4d2

---
+++ week4/wk4d2_Lab4.py
@@ -0,0 +1,21 @@
+#Jayda Bowe
+#Jan 30 2025
+#W4D2 - This is a two-part program where you will work on creating and populating parallel lists based on file data, then create and write data to a file.
+
+#Variable Dictionary
+
+
+#--IMPORTS------------------------------------------------------------------------------------------------------------
+import csv
+
+#--FUNCTIONS-----------------------------------------------------------------------------------------------------------
+
+
+#--MAIN EXECUTING CODE--------------------------------------------------------------------------------------------------
+
+firstname = []
+lastname = []
+age = []
+sceenname = []
+house = []
+
